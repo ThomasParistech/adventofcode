cb337f68b41708c2e4d9c1254b30529da470e1a9
Thomas Rouch <thom01.rouch@gmail.com>
2021-12-03 17:15:35 +0100

    day 1 part two

--- day_1.py
+++ day_1.py
@@ -4,10 +4,8 @@
 import pandas as pd
 
 
-def part_one():
-    input_path = '/home/trouch/Dev/adventofcode2021/day_1a.csv'
-
-    reader = pd.read_csv(input_path, delimiter=',', chunksize=100, header=None,
+def part_one(path):
+    reader = pd.read_csv(path, delimiter=',', chunksize=100, header=None,
                          names=['depth'], dtype={'depth': int})
 
     last_depth = None
@@ -19,3 +17,31 @@ def part_one():
             last_depth = d
 
     print(n_increases)
+
+
+def part_two(path):
+    reader = pd.read_csv(path, delimiter=',', chunksize=100, header=None,
+                         names=['depth'], dtype={'depth': int})
+
+    last_depth = None
+    last_last_depth = None
+    last_sum = None
+    n_increases = 0
+    for chunk in reader:
+        for d in chunk["depth"]:
+            if last_last_depth is not None and last_depth is not None:
+                sum = d + last_depth + last_last_depth
+                if last_sum is not None and sum > last_sum:
+                    n_increases += 1
+                last_sum = sum
+
+            last_last_depth = last_depth
+            last_depth = d
+
+    print(n_increases)
+
+
+if __name__ == "__main__":
+    input_path = '/home/trouch/Dev/adventofcode2021/day_1a.csv'
+    part_one(input_path)
+    part_two(input_path)
