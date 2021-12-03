a7257bf016be07cb3f5480888b9694a982e83dcb
Thomas Rouch <thom01.rouch@gmail.com>
2021-12-03 17:06:52 +0100

    clean

--- day_1.py
+++ day_1.py
@@ -3,17 +3,19 @@
 
 import pandas as pd
 
-input_path = '/home/trouch/Dev/adventofcode2021/day_1a.csv'
 
-reader = pd.read_csv(input_path, delimiter=',', chunksize=100, header=None,
-                     names=['depth'], dtype={'depth': int})
+def part_one():
+    input_path = '/home/trouch/Dev/adventofcode2021/day_1a.csv'
 
-last_depth = None
-n_increases = 0
-for chunk in reader:
-    for d in chunk["depth"]:
-        if last_depth is not None and d > last_depth:
-            n_increases += 1
-        last_depth = d
+    reader = pd.read_csv(input_path, delimiter=',', chunksize=100, header=None,
+                         names=['depth'], dtype={'depth': int})
 
-print(n_increases)
+    last_depth = None
+    n_increases = 0
+    for chunk in reader:
+        for d in chunk["depth"]:
+            if last_depth is not None and d > last_depth:
+                n_increases += 1
+            last_depth = d
+
+    print(n_increases)
