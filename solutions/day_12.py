8164a90b8e9b013655329169bb061433485f3947
Thomas Rouch <thom01.rouch@gmail.com>
2021-12-12 23:43:52 +0100

    part 12 a

--- solutions/day_12.py
+++ solutions/day_12.py
@@ -8,32 +8,51 @@ import numpy as np
 def _read_lines(path):
     with open(path) as f:
         all_rows = f.readlines()
-        all_rows = [list(row.strip()) for row in all_rows]
+        paths = [tuple(row.strip().split('-')) for row in all_rows]
 
-        np_rows = np.array(all_rows, dtype=int)
-        return np_rows
+        possibilities = {}
+
+        for (a, b) in paths:
+            if a in possibilities:
+                possibilities[a].append(b)
+            else:
+                possibilities[a] = [b]
+
+            if b in possibilities:
+                possibilities[b].append(a)
+            else:
+                possibilities[b] = [a]
+
+        return possibilities  # Cave Name -> List of available cave names
+
+
+def propagate(possibilities, path_counts: int, path: List[str]) -> int:
+    if path[-1] == "end":
+        print(",".join(path))
+        return path_counts + 1
+
+    next_caves = possibilities[path[-1]]
+    for next_cave in next_caves:
+        if (next_cave.lower() == next_cave) and next_cave in path:
+            continue
+
+        path.append(next_cave)
+        path_counts = propagate(possibilities, path_counts, path)
+        path.pop()
+
+    return path_counts
 
 
 def part_one(path: str) -> int:
-    #
-    #
-    #
-    #
-    #
-    #
-    #     TODO
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    return 0
+    possibilities = _read_lines(path)
+
+    path = ["start"]
+    path_counts = propagate(possibilities, 0, path)
+
+    return path_counts
 
 
 def part_two(path: str) -> int:
-    #
     #
     #
     #
