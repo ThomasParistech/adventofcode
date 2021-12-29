250383be3539383ec049ac5851f73f758e574e79
Thomas Rouch <thom01.rouch@gmail.com>
2021-12-29 13:37:10 +0100

    day 21 part 2 in 5ms

--- solutions/day_21.py
+++ solutions/day_21.py
@@ -1,11 +1,11 @@
 # /usr/bin/python3
 """Day 21."""
 
-from typing import List, Set, Tuple
+from typing import Dict, List, Set, Tuple
 import numpy as np
 
 
-class Dice:
+class DeterministicDice:
     def __init__(self):
         self.next_val = 1
         self.n_rolls = 0
@@ -34,12 +34,13 @@ def _read_lines(path):
     with open(path) as f:
         all_rows = f.readlines()
         one, two = tuple(int(row.strip().split(": ")[-1]) for row in all_rows)
-        return Player(one), Player(two)
+        return one, two
 
 
 def part_one(path: str) -> int:
-    player_1, player_2 = _read_lines(path)
-    dice = Dice()
+    one, two = _read_lines(path)
+    player_1, player_2 = Player(one), Player(two)
+    dice = DeterministicDice()
 
     while True:
         dice_sum = dice.next(3)
@@ -51,22 +52,56 @@ def part_one(path: str) -> int:
         if win_2:
             return player_1.score * dice.n_rolls
 
-    return -1
+
+def get_3_sums():
+    counts = {k: 0 for k in range(3, 10)}
+    for a in range(1, 4):
+        for b in range(1, 4):
+            for c in range(1, 4):
+                counts[a+b+c] += 1
+    return counts
+
+
+COUNTS_3_SUMS = get_3_sums()  # How many universes per sum result
+
+
+class DiracPlayer:
+    def __init__(self, init_pos: int):
+        # [pos][score] = n_univ
+        self.universes: List[Dict[int, int]] = [{} for _ in range(11)]
+        self.universes[init_pos][0] = 1
+        self.n_wins = 0
+
+    def next(self, other: 'DiracPlayer') -> bool:
+        new_universes = [{} for _ in range(11)]
+        n_winning_universes = 0
+        for pos, dico in enumerate(self.universes):
+            for score, n_univ in dico.items():
+                for sum3, count_univ in COUNTS_3_SUMS.items():
+                    new_pos = 1 + (pos+sum3-1) % 10
+                    new_score = score+new_pos
+                    new_n_univ = n_univ*count_univ
+                    if new_score >= 21:
+                        n_winning_universes += new_n_univ
+                    else:
+                        new_universes[new_pos][new_score] = new_universes[new_pos].get(new_score, 0) + new_n_univ
+        self.universes = new_universes
+
+        self.n_wins += n_winning_universes * other.get_n_remaining_universes()
+        return self.get_n_remaining_universes() != 0
+
+    def get_n_remaining_universes(self):
+        return int(np.sum(np.sum(list(dico.values())) for dico in self.universes))
 
 
 def part_two(path: str) -> int:
-    #
-    #
-    #
-    #
-    #
-    #
-    #     TODO
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    return 0
+    one, two = _read_lines(path)
+    player_1, player_2 = DiracPlayer(one), DiracPlayer(two)
+
+    while True:
+        if not player_1.next(player_2):
+            break
+        if not player_2.next(player_1):
+            break
+
+    return player_1.n_wins
