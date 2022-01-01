b109cd94aa5f1cbca8b32b0cc609ce67e5439f0e
Thomas Rouch <thom01.rouch@gmail.com>
2022-01-01 23:40:00 +0100

    faster day 24

--- solutions/day_24.py
+++ solutions/day_24.py
@@ -1,7 +1,7 @@
 # /usr/bin/python3
 """Day 24."""
 
-from typing import List, Optional
+from typing import List, Optional, Tuple
 import numpy as np
 
 
@@ -12,66 +12,41 @@ def _read_lines(path):
         alphas = [int(row.split(" ")[-1]) for row in all_rows[[idx + 4 for idx in inp_indices]]]
         betas = [int(row.split(" ")[-1]) for row in all_rows[[idx + 5 for idx in inp_indices]]]
         gammas = [int(row.split(" ")[-1]) for row in all_rows[[idx + 15 for idx in inp_indices]]]
+        print(betas)
+        return list(zip(alphas, betas, gammas))
 
-        return alphas, betas, gammas
 
+def rec(read_idx: int, alphas_betas_gammas: List[Tuple[int, int, int]],
+        small_first: bool, z: int = 0, nbr: str = "") -> Optional[str]:
+    # Idea: When alpha=26 do //26 and when alpha=1 do *26
 
-class RevertNode:
-    def __init__(self, z: int, depth: int):
-        self.z: int = z
-        self.depth: int = depth
+    if read_idx == len(alphas_betas_gammas):
+        return nbr if z == 0 else None
 
-    def find_parents(self, small_first: bool, nbr: str, alphas: List[int], betas: List[int], gammas: List[int]) -> Optional[int]:
-        prev_depth = self.depth - 1
-        if prev_depth == -1:
-            return int(nbr)
+    alpha, beta, gamma = alphas_betas_gammas[read_idx]
 
-        alpha = alphas[prev_depth]
-        beta = betas[prev_depth]
-        gamma = gammas[prev_depth]
-
-        a_z_to_add = []
-        if alpha == 1:
-            # Failing test
-            prev_z = self.z // 26
-            prev_a = self.z % 26 - gamma
-            if (prev_a != beta + prev_z % 26) and (1 <= prev_a <= 9):
-                a_z_to_add.append((prev_a, prev_z))
-
-            # Passing test
-            prev_z = self.z
-            prev_a = beta + prev_z % 26
-            if 1 <= prev_a <= 9:
-                a_z_to_add.append((prev_a, prev_z))
-
-        else:  # alpha=26
-            # Failing test
-            prev_a = self.z % 26 - gamma
-            if 1 <= prev_a <= 9:
-                core_prev_z = 26*(self.z // 26)
-                for mod_z in range(26):
-                    prev_z = core_prev_z + mod_z
-                    if (prev_a != beta + prev_z % 26):
-                        a_z_to_add.append((prev_a, prev_z))
-            # Passing test
-            a_range = range(max(1, beta), 10) if small_first else range(9, max(0, beta-1), -1)
-            for prev_a in a_range:
-                prev_z = 26*self.z + prev_a - beta
-                a_z_to_add.append((prev_a, prev_z))
-
-        for prev_a, prev_z in a_z_to_add:
-            parent = RevertNode(prev_z, prev_depth)
-            res = parent.find_parents(small_first, f"{prev_a}{nbr}", alphas, betas, gammas)
-            if res is not None:
-                return res
+    if alpha == 26:
+        a = z % 26 + beta
+        if (1 <= a <= 9):
+            return rec(read_idx+1, alphas_betas_gammas, small_first, z//26, f"{nbr}{a}")
         return None
+
+    assert alpha == 1
+    core_z = z*26+gamma
+    a_range = range(1, 10) if small_first else reversed(range(1, 10))
+    a_forbidden = beta + z % 26
+    for a in a_range:
+        if a != a_forbidden:
+            result = rec(read_idx+1, alphas_betas_gammas, small_first, core_z+a, f"{nbr}{a}")
+            if result is not None:
+                return result
 
 
 def part_one(path: str) -> int:
-    alphas, betas, gammas = _read_lines(path)
-    return RevertNode(0, 14).find_parents(False, "", alphas, betas, gammas)
+    abg = _read_lines(path)
+    return int(rec(0, abg, small_first=False))
 
 
 def part_two(path: str) -> int:
-    alphas, betas, gammas = _read_lines(path)
-    return RevertNode(0, 14).find_parents(True, "", alphas, betas, gammas)
+    abg = _read_lines(path)
+    return int(rec(0, abg, small_first=True))
