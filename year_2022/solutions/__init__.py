3819fe077fa63654452b57054ccb2784cda7bba9
Thomas Rouch <thom01.rouch@gmail.com>
2023-01-20 15:32:36 +0100

    day 1 part2

--- year_2022/solutions/__init__.py
+++ year_2022/solutions/__init__.py
@@ -7,5 +7,5 @@ from typing import List, Tuple
 # Day -> (Part1, Part2)
 EXPECTED_TOY_SOLUTIONS: List[Tuple[int, int]] = [
     (None, None),
-    (24000, None)
+    (24000, 45000)
 ]
--- year_2022/solutions/day_01.py
+++ year_2022/solutions/day_01.py
@@ -7,14 +7,16 @@ import numpy as np
 
 def _read_lines(path: str) -> List[List[int]]:
     with open(path) as f:
-        return [[int(row.strip()) for row in rows.split("\n")] for rows in f.read().split("\n\n")]
+        counts = [[int(row.strip()) for row in rows.split("\n")] for rows in f.read().split("\n\n")]
+        return [np.sum(c) for c in counts]
 
 
 def part_one(path: str) -> int:
-    counts = _read_lines(path)
-    aggregated_counts = [np.sum(c) for c in counts]
+    aggregated_counts = _read_lines(path)
     return np.max(aggregated_counts)
 
 
 def part_two(path: str) -> int:
-    return -1
+    aggregated_counts = _read_lines(path)
+    top_three = np.argsort(aggregated_counts)[-3:]
+    return np.sum(np.array(aggregated_counts)[top_three])
