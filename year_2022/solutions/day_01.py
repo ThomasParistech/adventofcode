4e25a9e3f87325da2f16d6486906137f71cd672c
Thomas Rouch <thom01.rouch@gmail.com>
2023-01-20 15:28:48 +0100

    day1 part 1

--- year_2022/solutions/day_01.py
+++ year_2022/solutions/day_01.py
@@ -1,11 +1,19 @@
 # /usr/bin/python3
 """Day 1."""
 
-import pandas as pd
+from typing import List
+import numpy as np
+
+
+def _read_lines(path: str) -> List[List[int]]:
+    with open(path) as f:
+        return [[int(row.strip()) for row in rows.split("\n")] for rows in f.read().split("\n\n")]
 
 
 def part_one(path: str) -> int:
-    return -1
+    counts = _read_lines(path)
+    aggregated_counts = [np.sum(c) for c in counts]
+    return np.max(aggregated_counts)
 
 
 def part_two(path: str) -> int:
